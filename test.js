792e809a6b269de376cd0c571aa0cc9f32b1d9c3
John Otander <johnotander@gmail.com>
2015-12-10 15:04:26 -0700

    Fix tests

--- test.js
+++ test.js
@@ -2,23 +2,11 @@ import fs from 'fs'
 import test from 'ava'
 import pify from 'pify'
 import childProcess from 'child_process'
+import pkg from './package.json'
 
-const expected = fs.readFileSync('expected.html', 'utf8')
-const fixtureFile = 'fixture.html'
-const outputFile = 'output.html'
-
-test('strips html comments', async t => {
+test('gets the latest version', async t => {
   t.plan(1)
 
-  const stdout = await pify(childProcess.execFile)('./cli.js', [fixtureFile])
-  t.same(stdout, expected)
-})
-
-test('writes to a file when provided', async t => {
-  t.plan(1)
-
-  await pify(childProcess.execFile)('./cli.js', [fixtureFile, outputFile])
-  const output = fs.readFileSync(outputFile, 'utf8')
-
-  t.same(output, expected)
+  const stdout = await pify(childProcess.execFile)('./cli.js', ['johnotander', 'github-latest-cli'])
+  t.same(stdout.trim(), 'v' + pkg.version)
 })
